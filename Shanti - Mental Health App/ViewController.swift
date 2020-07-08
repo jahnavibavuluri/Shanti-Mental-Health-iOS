cc62c49d003255c72eede6a9727dd35efd6ff4b3
Jahnavi Bavuluri <jbavuluri@gmail.com>
2020-07-08 01:00:05 -0400

    Initial Commit

---
+++ Shanti - Mental Health App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Shanti - Mental Health App
+//
+//  Created by Jahnavi Bavuluri  on 7/8/20.
+//  Copyright © 2020 Jahnavi Bavuluri . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
